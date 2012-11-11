9648934e1d2ae4967c8e7e379e5d0ba30657cdd5
yujiorama <yujiorama+github@gmail.com>
2012-11-11 17:40:44 +0900

    add todo

--- mysvnstat.rb
+++ mysvnstat.rb
@@ -6,11 +6,16 @@ require 'haml'
 class MySvnstat < Sinatra::Base
   enable :inline_templates
   enable :logging
+  set :haml, {:format => :html5}
   set :server, 'webrick'
   register Sinatra::Contrib
   
   get '/' do
     @title = "main"
+    haml :index
+  end
+
+  post '/upload' do
     haml :index
   end
 end
